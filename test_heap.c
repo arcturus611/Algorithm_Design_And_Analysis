ba0921caa9002a51125ddb25096ffc9c7299af2d
arcturus611 <swati.6.itaws@gmail.com>
2015-03-27 00:04:11 -0700

    heap test code

---
+++ test_heap.c
@@ -0,0 +1,18 @@
+	/*
+		Testing heap code
+	*/
+	
+	#include<stdio.h>
+	#include<stdlib.h>
+	#include "heap.h"
+	
+	int main(int argc, char* argv[]){
+		struct heap* H = init_heap();
+		print_heap(H); 
+		for(int i = 5; i>0; i--){
+			insert_in_heap(H, i); 
+			print_heap(H);
+		}
+		print_heap(H);
+		return 0;
+	}
